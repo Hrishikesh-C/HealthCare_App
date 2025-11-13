a2f0a1fae0b6393cb347fed231eb6b77daae4344
saich <saich@sai>
2025-11-13 08:05:18 +0530

    new commit

--- src/com/healthcare/Patient.java
+++ src/com/healthcare/Patient.java
@@ -4,6 +4,7 @@ public class Patient {
 	void disease() {
 		System.out.println("suffering from fever");
 		System.out.println("still suffering but maybe recovered");
+		System.out.println("completely recovered");
 	}
 	
 }
