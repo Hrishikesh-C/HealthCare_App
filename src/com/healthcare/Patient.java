7d581c62954247620d81ca60184b5a8a5f3f7ca3
saich <saich@sai>
2025-11-13 08:02:15 +0530

    third commit

--- src/com/healthcare/Patient.java
+++ src/com/healthcare/Patient.java
@@ -3,6 +3,7 @@ package com.healthcare;
 public class Patient {
 	void disease() {
 		System.out.println("suffering from fever");
+		System.out.println("still suffering but maybe recovered");
 	}
 	
 }
